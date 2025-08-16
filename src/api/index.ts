17ed6edceea5d4333f4b6b0bfea5813b306d24b1
ridwanmuh3 <ridwanmuhraihan@gmail.com>
2025-08-16 11:11:45 +0700

    fix: fixing refresh token interceptors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,14 @@
-import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosError, type AxiosRequestConfig } from "axios";
 
-type AxiosConfig = InternalAxiosRequestConfig & {
-  _retry: boolean;
+type RetryQueueItem = {
+  resolve: (value?: any) => void;
+  reject: (error?: any) => void;
+  config: AxiosRequestConfig;
 };
+
+const refreshAndRetryQueue: RetryQueueItem[] = [];
+
+let isRefreshing = false;
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:9000/api/v1",
@@ -25,28 +31,50 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
-    const originalRequest: AxiosConfig = error.config as AxiosConfig;
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const refreshToken = localStorage.getItem("refresh_token");
-        const response = await axios.post("/auth/refresh", {
-          refreshToken,
-        });
-        const { access_token, refresh_token: newRefreshToken } = response.data;
-        localStorage.setItem("access_token", access_token);
-        localStorage.setItem("refresh_token", newRefreshToken);
-        axiosInstance.defaults.headers.common["Authorization"] =
-          `Bearer ${access_token}`;
+    const originalRequest: AxiosRequestConfig =
+      error.config as AxiosRequestConfig;
 
-        return axiosInstance(originalRequest);
-      } catch (refreshError) {
-        console.error("token refresh failed", refreshError);
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        window.location.href = "/login";
-        return Promise.reject(refreshError);
+    if (error.response && error.response?.status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          const refreshToken = localStorage.getItem("refresh_token");
+          const response = await axios.post("/auth/refresh", {
+            refreshToken,
+          });
+          const { access_token, refresh_token: newRefreshToken } =
+            response.data;
+
+          localStorage.setItem("access_token", access_token);
+          localStorage.setItem("refresh_token", newRefreshToken);
+
+          axiosInstance.defaults.headers.common["Authorization"] =
+            `Bearer ${access_token}`;
+
+          refreshAndRetryQueue.forEach(({ config, resolve, reject }) => {
+            axiosInstance
+              .request(config)
+              .then((response) => resolve(response))
+              .catch((err) => reject(err));
+          });
+
+          refreshAndRetryQueue.length = 0;
+
+          return axiosInstance(originalRequest);
+        } catch (refreshError) {
+          console.error("token refresh failed", refreshError);
+          localStorage.removeItem("access_token");
+          localStorage.removeItem("refresh_token");
+          window.location.href = "/login";
+          throw refreshError;
+        } finally {
+          isRefreshing = false;
+        }
       }
+
+      return new Promise<void>((resolve, reject) => {
+        refreshAndRetryQueue.push({ config: originalRequest, resolve, reject });
+      });
     }
     return Promise.reject(error);
   },
